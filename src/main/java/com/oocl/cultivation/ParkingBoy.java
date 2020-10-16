2e4fb5b8d75e1d3f2ad29f2489170e25ccf02af4
bryanobmasca <bryanobmasca@gmail.com>
2020-10-16 14:21:08 +0800

    [ParkingLot][Bryan] feature: Implement park and fetch using hashMap

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,14 +1,18 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
+
+    private final ParkingLot parkingLot;
+
     public ParkingBoy(ParkingLot parkingLot) {
+        this.parkingLot = parkingLot;
     }
 
     public ParkingTicket park(Car car) {
-        return new ParkingTicket();
+        return parkingLot.park(car);
     }
 
     public Car fetch(ParkingTicket parkingTicket) {
-        return null;
+        return parkingLot.fetch(parkingTicket);
     }
 }
