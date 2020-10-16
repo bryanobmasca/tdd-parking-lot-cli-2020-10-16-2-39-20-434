c73baf59f9d2d9445d25708ae9789c6c6c1ee42f
bryanobmasca <bryanobmasca@gmail.com>
2020-10-16 13:56:28 +0800

    [ParkingLot][Bryan] test: Add a test for parking boy to park a car and return a ticket

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,17 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
-
+    public void should_return_parking_ticket_when_parked_given_a_car_to_parking_boy() {
+        //given
+        Car car = new Car();
+        ParkingBoy parkingBoy = new ParkingBoy(new ParkingLot());
+        //when
+        ParkingTicket ticket = parkingBoy.park(car);
+        //then
+        assertNotNull(ticket);
     }
 }
