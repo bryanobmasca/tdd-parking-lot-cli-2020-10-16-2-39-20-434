e044096ca466b480c319c9540ebc8a1ae8bb969f
bryanobmasca <bryanobmasca@gmail.com>
2020-10-19 20:38:35 +0800

    [ParkingLot Story5][Bryan] refactor: change class test method to private

--- src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SuperSmartParkingBoyTest {
+class SuperSmartParkingBoyTest {
     @Test
-    public void should_park_in_larger_available_slot_when_parking_given_first_level_capacity_is_2_having_1_parked_car_and_second_level_capacity_is_3_having_no_parked_car() {
+    void should_park_in_larger_available_slot_when_parking_given_first_level_capacity_is_2_having_1_parked_car_and_second_level_capacity_is_3_having_no_parked_car() {
         //given
         Car car1 = new Car();
         Car car2 = new Car();
