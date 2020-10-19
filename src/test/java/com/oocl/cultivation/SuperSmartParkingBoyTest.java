8ef55e3b867abf94360d37404cbe14903f8f4a5b
bryanobmasca <bryanobmasca@gmail.com>
2020-10-19 20:26:24 +0800

    [ParkingLot Story5][Bryan] test: Add a test to park in larger available slot when parking

---
+++ src/test/java/com/oocl/cultivation/SuperSmartParkingBoyTest.java
@@ -0,0 +1,24 @@
+package com.oocl.cultivation;
+
+import org.junit.jupiter.api.Test;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SuperSmartParkingBoyTest {
+    @Test
+    public void should_park_in_larger_available_slot_when_parking_given_first_level_capacity_is_2_having_1_parked_car_and_second_level_capacity_is_3_having_no_parked_car() {
+        //given
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+        SuperSmartParkingBoy superSmartParkingBoy = new SuperSmartParkingBoy(asList(new ParkingLot(2), new ParkingLot(3)));
+        //when
+        superSmartParkingBoy.park(car1);
+        superSmartParkingBoy.park(car2);
+        superSmartParkingBoy.park(car3);
+        //then
+        assertEquals(1, superSmartParkingBoy.getParkingLotArrayList().get(0).getParkedCarsSize());
+        assertEquals(2, superSmartParkingBoy.getParkingLotArrayList().get(1).getParkedCarsSize());
+    }
+}
