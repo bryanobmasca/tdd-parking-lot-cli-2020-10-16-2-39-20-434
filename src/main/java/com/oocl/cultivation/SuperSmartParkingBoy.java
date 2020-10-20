4c62fd49e7ff8accf7e1c764df777d84ac894721
bryanobmasca <bryanobmasca@gmail.com>
2020-10-20 20:49:06 +0800

    [Strategy][Bryan] refactor: Modify SuperSmartParkingBoy to use the strategy

--- src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
+++ src/main/java/com/oocl/cultivation/SuperSmartParkingBoy.java
@@ -1,21 +1,18 @@
 package com.oocl.cultivation;
 
-import com.oocl.cultivation.Exception.NoAvailableSlotException;
+import com.oocl.cultivation.Strategy.LargestAvailableRate;
 
-import java.util.Comparator;
 import java.util.List;
 
 public class SuperSmartParkingBoy extends ParkingBoy {
 
     public SuperSmartParkingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
+        parking = new LargestAvailableRate();
     }
 
     @Override
     public ParkingTicket park(Car car) {
-        return parkingLots.stream()
-                .max(Comparator.comparingDouble(ParkingLot::getAvailablePositionRate))
-                .orElseThrow(NoAvailableSlotException::new)
-                .park(car);
+        return parking.park(car, parkingLots);
     }
 }
