a02008de3b81e4b0a1e3dc7fb67099c59d36b66a
bryanobmasca <bryanobmasca@gmail.com>
2020-10-20 19:35:56 +0800

    [ParkingLot Story6][Bryan] test: Add a test to add one parking boy in the management list

---
+++ src/main/java/com/oocl/cultivation/ServiceManager.java
@@ -0,0 +1,13 @@
+package com.oocl.cultivation;
+
+import java.util.List;
+
+public class ServiceManager {
+    public void addToManagementList(ParkingBoy parkingBoy) {
+
+    }
+
+    public List<ParkingBoy> getManagementList() {
+        return null;
+    }
+}
